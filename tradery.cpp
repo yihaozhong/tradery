679b15b5557e36f61f70ca1052eba7ffeb4d354c
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-08-30 01:10:37 -0400

    set up framework and packages

---
+++ tradery.cpp
@@ -0,0 +1,54 @@
+// Include necessary headers
+#include <iostream>
+#include <thread>
+#include <boost/asio.hpp>
+#include <quickfix/SocketInitiator.h>
+
+// Tradery class
+class Tradery {
+public:
+    Tradery() {
+        // Initialize components such as order book, market data, FIX engine
+    }
+    
+    void start() {
+        // Start the trading system, fetch market data, and process orders
+    }
+};
+
+// OrderBook class
+class OrderBook {
+    // Lock-free data structures and memory pooling can be implemented here
+public:
+    void addOrder(/*order details*/) {
+        // Add order to the book
+    }
+    
+    void matchOrder(/*order details*/) {
+        // Match orders and execute trades
+    }
+};
+
+// MarketData class
+class MarketData {
+public:
+    void fetch() {
+        // Fetch market data from providers using FIX or other protocols
+    }
+};
+
+// FixEngine class
+class FixEngine {
+public:
+    void sendMessage(/*message details*/) {
+        // Send FIX messages using quickfix or fix8
+    }
+};
+
+// Main function
+int main() {
+    Tradery tradery;
+    tradery.start();
+
+    return 0;
+}
