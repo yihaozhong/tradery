6aa5ccf2e7c3d7a8a433c52c575671610614719c
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-08-31 16:54:14 -0400

    restructure

--- tradery.cpp
+++ tradery.cpp
@@ -1,55 +1,90 @@
 // Include necessary headers
 #include <iostream>
 #include <thread>
+#include <vector>
 #include <boost/asio.hpp>
+#include <boost/math/distributions/normal.hpp>
 #include <quickfix/SocketInitiator.h>
 
-// Tradery class
+class OptionPricer {
+    // ... (same as before)
+};
+
+class OptionOrder {
+    // Basic order details here
+};
+
+class OptionOrderBook {
+private:
+    std::vector<OptionOrder> buyOrders;
+    std::vector<OptionOrder> sellOrders;
+
+public:
+    void addOrder(const OptionOrder& order) {
+        // Add order to appropriate side of the book
+    }
+    
+    void matchOrder() {
+        // Match orders and execute trades
+    }
+};
+
+class MarketData {
+    // Fetch market data and store it
+};
+
+class FixEngine {
+    // Handle FIX protocol messaging
+};
+
 class Tradery {
+private:
+    OptionPricer pricer;
+    OptionOrderBook orderBook;
+    MarketData marketData;
+    FixEngine fixEngine;
+
+    double spreadPercentage = 0.02;  // Example spread percentage
+
 public:
     Tradery() {
         // Initialize components
     }
     
-    void start() {
-        // Start the trading system
-    }
-};
+    void makeMarket() {
+        // Fetch the latest market data
+        marketData.fetch();
 
-// OptionOrderBook class
-class OptionOrderBook {
-public:
-    void addOrder(/*order details*/) {
-        // Add order to the book
+        // Use market data to get parameters for pricing
+        // Example:
+        double S = 100;  // Current stock price
+        double K = 105;  // Strike price
+        double T = 0.5;  // Time to expiration (in years)
+        double r = 0.03; // Risk-free rate
+        double q = 0.01; // Dividend yield
+        double sigma = 0.2; // Volatility
+
+        double theoreticalPrice = pricer.callPrice(S, K, T, r, q, sigma);
+        double bidPrice = theoreticalPrice * (1 - spreadPercentage);
+        double askPrice = theoreticalPrice * (1 + spreadPercentage);
+
+        // Use bidPrice and askPrice to place orders in the market
+        OptionOrder bidOrder;  // Create bid order with bidPrice
+        OptionOrder askOrder;  // Create ask order with askPrice
+
+        orderBook.addOrder(bidOrder);
+        orderBook.addOrder(askOrder);
+        
+        // Try to match orders in the book
+        orderBook.matchOrder();
     }
     
-    void matchOrder(/*order details*/) {
-        // Match orders and execute trades
-    }
-};
-
-// MarketData class
-class MarketData {
-public:
-    void fetch() {
-        // Fetch market data from providers
-    }
-};
-
-// FixEngine class
-class FixEngine {
-public:
-    void sendMessage(/*message details*/) {
-        // Send FIX messages
-    }
-};
-
-// OptionPricer class
-class OptionPricer {
-public:
-    double calculate(/*option parameters*/) {
-        // Calculate option price using Black-Scholes or another model
-        return 0.0; // Placeholder
+    void start() {
+        // Start the trading system
+        while (true) {  // Example infinite loop for continuous market making
+            makeMarket();
+            std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Sleep for 100 ms
+        }
     }
 };
 
